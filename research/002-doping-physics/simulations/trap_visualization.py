987993064f1a43b7a11a55d94c6848bbef22f040
Khushpreet Singh <khushaulakhind@gmail.com>
2026-05-03 20:38:14 +0530

    Fix NameError: Define eta in visualization script

--- research/002-doping-physics/simulations/trap_visualization.py
+++ research/002-doping-physics/simulations/trap_visualization.py
@@ -3,8 +3,9 @@ import matplotlib.pyplot as plt
 Ei = np.linspace(0.01, 0.6, 100) 
 T = 300 
 kb = 8.617e-5 
+eta = 1 / (1 + 2 * np.exp(Ei / (kb * T))) 
 plt.plot(Ei, eta) 
 plt.xlabel('Ionization Energy (eV)') 
 plt.ylabel('Dopant Efficiency') 
-plt.title('The OPRC Trap-Transition Curve') 
+plt.title('OPRC Trap-Transition Curve') 
 plt.savefig('research/002-doping-physics/verdicts/trap_curve.png') 
